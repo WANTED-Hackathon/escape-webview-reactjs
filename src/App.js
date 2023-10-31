8b31219414446b583b807c63317edc0da2518ce7
lambiengcode <lambiengcode@gmail.com>
2023-10-31 22:27:53 +0700

    update .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const openInBrowser = () => {
+    // Replace 'https://example.com' with the URL you want to open in the browser
+    const url = 'https://askany.com';
+
+    // Customize the user agent (optional)
+    const customUserAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36';
+
+    // Save the current user agent
+    const originalUserAgent = window.navigator.userAgent;
+
+    // Override the user agent
+    Object.defineProperty(window.navigator, 'userAgent', {
+      value: customUserAgent,
+      configurable: true,
+    });
+
+    // Open the URL in the user's default browser
+    window.open(url, '_blank');
+
+    // Restore the original user agent (optional)
+    Object.defineProperty(window.navigator, 'userAgent', {
+      value: originalUserAgent,
+      configurable: true,
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +36,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={openInBrowser}>Open in Browser</button>
       </header>
     </div>
   );
